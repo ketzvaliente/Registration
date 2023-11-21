106a25f3975ba68000518a0b1c99d451ef40dd59
ketzvaliente <151612788+ketzvaliente@users.noreply.github.com>
2023-11-21 15:37:38 +0800

    Add files via upload

---
+++ valiente/src/main/java/com/mycompany/valiente/Valiente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.valiente;
+
+/**
+ *
+ * @author WS
+ */
+public class Valiente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
